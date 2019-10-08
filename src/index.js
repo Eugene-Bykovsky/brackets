ed273daee9d0679050d2419908f6bb54caccd0ea
EugeneBykovsky <bykovsky.ssau@gmail.com>
2019-10-08 13:48:51 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let data = str.split(''),
+      config = bracketsConfig,
+      stack = [],
+      current = [];
+
+  data.forEach(element => {
+    for (let i = 0; i < config.length; i++) {
+      if (config[i].indexOf(element) != -1) {
+        if (config[i][0] === element 
+          & config[i][1] === element 
+          & stack.indexOf(element) != -1) {
+          if (i === current[current.length - 1]) {
+            stack.pop();
+            current.pop();
+          }
+        } else if (config[i][0] === element) {
+          stack.push(element);
+          current.push(i);
+        } else if (i === current[current.length - 1] 
+          & config[i][1] === element 
+          & stack.indexOf(config[i][0]) != -1) {
+          stack.pop();
+          current.pop();
+        } else {
+          stack.push(element);
+          current.push(i);
+        }
+      }
+    }
+  });
+
+  let result = (stack.length === 0) ? true : false;
+  return result;
 }
